c82e4f61d3a87806d38864a406d07e25b70e7809
John Deng Madit Akech <127344777+John-Akech@users.noreply.github.com>
2024-09-14 18:38:13 +0200

    Delete math/linear_algebra/100-slice_like_a_ninja.py

--- math/linear_algebra/100-slice_like_a_ninja.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import numpy as np
-
-def np_slice(matrix, axes={}):
-    # Create a tuple with slices based on the provided axes
-    slices = tuple(slice(None) if i not in axes else slice(*axes[i]) for i in range(matrix.ndim))
-    return matrix[slices]
