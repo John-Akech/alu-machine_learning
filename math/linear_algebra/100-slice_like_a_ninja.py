d727f34095b307c13d720b0440329f7b2374b3ed
John Deng Madit Akech <127344777+John-Akech@users.noreply.github.com>
2024-09-14 20:19:24 +0200

    Update 100-slice_like_a_ninja.py

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 """ Implements the np_slice method
 """
-import numpy as np
 
 def np_slice(matrix, axes={}):
     """ Slices a matrix along specified axes and limits
     """
     result = matrix
-    
+
+    {}.items()
+
     for axis, slice_params in axes.items():
         # Extract the slice params
         start, end, step = extract_slice_params(slice_params)
-        
-        # Generate slice object
-        slice_obj = slice(start, end, step)
-        
-        # Apply slicing along the specified axis
-        result = np.take(result, np.arange(result.shape[axis])[slice_obj], axis=axis)
-    
+
+        # Create indices to use to get data
+        indices = list(range(start, end, step))
+
+        # Get rows from start to end using step
+        result = result.take(indices, axis)
+
     return result
+
 
 def extract_slice_params(slice_tuple):
     start = end = step = None
@@ -31,7 +33,7 @@ def extract_slice_params(slice_tuple):
     elif len(slice_tuple) == 2:
         start, end = slice_tuple[0] or 0, slice_tuple[1] or -1
 
-    # If three values have been provided, then they are the start, end and step values
+    # If three values have been provided, then they are the start, end and stop values
     elif len(slice_tuple) == 3:
         start, end, step = slice_tuple[0] or 0, slice_tuple[1] or -1, slice_tuple[2] or 1
     
