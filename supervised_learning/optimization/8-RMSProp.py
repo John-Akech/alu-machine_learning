9bab133462d71f79940f9576e9d6cdbffd3cc2a2
John Akech <j.akech@alustudent.com>
2025-02-07 10:57:14 +0200

    Update 8-RMSProp.py

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
-""" Training with RMSProp
+""" Training with momentum
 """
 
 import tensorflow as tf
 
-def create_RMSProp_op(loss, alpha, beta2, epsilon, momentum=0.0):
-    """ Creates the training operation for a neural network in TensorFlow
-        using the RMSProp optimization algorithm.
 
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """ creates the training operation for a neural network in tensorflow
+        using the RMSProp optimization algorithm
         Args:
-            loss: The loss of the network.
-            alpha: The learning rate.
-            beta2: The decay rate for the moving average of the squared gradient.
-            epsilon: A small constant for numerical stability.
-            momentum: (Optional) Momentum parameter. Default is 0.0.
-
-        Returns:
-            The RMSProp optimization operation.
+            loss: is the loss of the network
+            alpha: is the learning rate
+            beta2: is the RMSProp weight
+            epsilon: is a small number to avoid division by zero
+        Returns: the RMSProp optimization operation
     """
-    optimizer = tf.optimizers.RMSprop(learning_rate=alpha, rho=beta2, epsilon=epsilon, momentum=momentum)
+
+    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon)
     return optimizer.minimize(loss)
