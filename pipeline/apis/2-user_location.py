4d1cf3913ec08434bd60922b0082140c208885e1
John-Akech <j.akech@alustudent.com>
2025-03-02 10:15:58 +0200

    Updated

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -7,18 +7,25 @@ import sys
 import time
 
 def get_user_location(api_url):
-    res = requests.get(api_url)
+    try:
+        res = requests.get(api_url)
 
-    if res.status_code == 403:
-        rate_limit_reset = int(res.headers.get('X-Ratelimit-Reset', 0))
-        current_time = int(time.time())
-        diff = (rate_limit_reset - current_time) // 60
-        print("Reset in {} min".format(diff))
-    elif res.status_code == 404:
-        print("Not found")
-    elif res.status_code == 200:
-        user_data = res.json()
-        print(user_data.get('location', 'Location not provided'))
+        if res.status_code == 403:
+            rate_limit_reset = int(res.headers.get('X-Ratelimit-Reset', 0))
+            current_time = int(time.time())
+            diff = (rate_limit_reset - current_time) // 60
+            print("Reset in {} min".format(diff))
+        elif res.status_code == 404:
+            print("Not found")
+        elif res.status_code == 200:
+            user_data = res.json()
+            # Print the location or a default message if not provided
+            print(user_data.get('location', 'Location not provided'))
+        else:
+            print("Error: Received status code {}".format(res.status_code))
+
+    except requests.exceptions.RequestException as e:
+        print("An error occurred: {}".format(e))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
