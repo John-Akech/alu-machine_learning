53b2eb76a6a590e34213dfee18368dc0dc789445
John Akech <j.akech@alustudent.com>
2025-03-02 09:19:52 +0200

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,40 +1,22 @@
 #!/usr/bin/env python3
-
-"""Fetch and print the location of a given GitHub user using the GitHub API"""
-
 import requests
 import sys
 import time
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./2-user_location.py <GitHub API URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
+    res = requests.get(url)
 
-    try:
-        res = requests.get(url)
+    if res.status_code == 403:
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset', 0))
+        current_time = int(time.time())
+        diff = (rate_limit - current_time) // 60
+        print(f"Reset in {diff} min")
+    
+    elif res.status_code == 404:
+        print("Not found")
 
-        if res.status_code == 403:
-            rate_limit = res.headers.get('X-Ratelimit-Reset')
-            if rate_limit:
-                reset_time = int(rate_limit)
-                current_time = int(time.time())
-                diff = (reset_time - current_time) // 60
-                print("Reset in {} min".format(diff))
-            else:
-                print("Rate limit exceeded, but reset time unavailable.")
-        
-        elif res.status_code == 404:
-            print("Not found")
-        
-        elif res.status_code == 200:
-            res_json = res.json()
-            print(res_json.get("location", "No location provided"))
-        
-        else:
-            print("Unexpected error: {}".format(res.status_code))
-
-    except requests.RequestException as e:
-        print("Error: {}".format(e))
+    elif res.status_code == 200:
+        data = res.json()
+        print("Full API Response:", data)  # Debugging line
+        print(data.get("location", "No location available"))  # Safer way to fetch location
