346916fd33d0c8b690ef7ae861e80837e993a46c
John Akech <j.akech@alustudent.com>
2025-03-02 09:03:57 +0200

    2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
-
-
 """ Return list of ships"""
 
-import requests
 import sys
+import requests
 import time
 
+def get_github_user_location(api_url):
+    response = requests.get(api_url)
+    
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data.get("location", "Location not available"))
+    elif response.status_code == 404:
+        print("Not found")
+    elif response.status_code == 403:
+        reset_time = int(response.headers.get("X-RateLimit-Reset", time.time()))
+        minutes_remaining = max(0, (reset_time - time.time()) // 60)
+        print(f"Reset in {int(minutes_remaining)} min")
+    else:
+        print(f"Error: {response.status_code}")
 
 if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
-
-    if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
-        current_time = int(time.time())
-        diff = (rate_limit - current_time) // 60
-        print("Reset in {} min".format(diff))
-        # get remaining rate
-
-    elif res.status_code == 404:
-        print("Not found")
-    elif res.status_code == 200:
-        res = res.json()
-        print(res['location'])
+    if len(sys.argv) != 2:
+        print("Usage: ./2-user_location.py <GitHub_API_URL>")
+        sys.exit(1)
+    
+    api_url = sys.argv[1]
+    get_github_user_location(api_url)
