afdf885297ae7696b40b7fa47e7360dfbb85acd0
John Akech <j.akech@alustudent.com>
2025-03-02 09:07:54 +0200

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -5,25 +5,25 @@ import sys
 import requests
 import time
 
-def get_github_user_location(api_url):
+def get_user_location(api_url):
     response = requests.get(api_url)
-    
-    if response.status_code == 200:
-        user_data = response.json()
-        print(user_data.get("location", "Location not available"))
-    elif response.status_code == 404:
+
+    if response.status_code == 404:
         print("Not found")
     elif response.status_code == 403:
-        reset_time = int(response.headers.get("X-RateLimit-Reset", time.time()))
-        minutes_remaining = max(0, (reset_time - time.time()) // 60)
-        print(f"Reset in {int(minutes_remaining)} min")
+        reset_time = int(response.headers.get('X-Ratelimit-Reset', 0))
+        current_time = int(time.time())
+        minutes_to_reset = (reset_time - current_time) // 60
+        print(f"Reset in {minutes_to_reset} min")
     else:
-        print(f"Error: {response.status_code}")
+        user_data = response.json()
+        location = user_data.get('location')
+        print(location if location else "Location not specified")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: ./2-user_location.py <GitHub_API_URL>")
+        print("Usage: ./2-user_location.py <API_URL>")
         sys.exit(1)
-    
+
     api_url = sys.argv[1]
-    get_github_user_location(api_url)
+    get_user_location(api_url)
