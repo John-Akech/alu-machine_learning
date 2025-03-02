f2e98fd9f2becb8b8cd5d35acbb8b60fa7db92fe
John Akech <j.akech@alustudent.com>
2025-03-02 09:35:01 +0200

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
+
+""" Return list of ships"""
+
 import requests
 import sys
 import time
 
+
 if __name__ == "__main__":
-    url = sys.argv[1]
-    res = requests.get(url)
+    res = requests.get(sys.argv[1])
 
     if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset', 0))
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
         current_time = int(time.time())
         diff = (rate_limit - current_time) // 60
-        print(f"Reset in {diff} min")
-    
+        print("Reset in {} min".format(diff))
+        # get remaining rate
+
     elif res.status_code == 404:
         print("Not found")
-
     elif res.status_code == 200:
-        data = res.json()
-        print("Full API Response:", data)  # Debugging line
-        print(data.get("location", "No location available"))  # Safer way to fetch location
+        res = res.json()
+        print(res['location'])
