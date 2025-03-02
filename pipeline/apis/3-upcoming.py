5161e6e775d049567915e7414c7c9b02d5cb8791
John Akech <j.akech@alustudent.com>
2025-03-02 10:52:34 +0200

    Update 3-upcoming.py

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
-
 """ Return list of ships"""
 
 import requests
 import sys
 import time
-
 
 if __name__ == "__main__":
     res = requests.get(sys.argv[1])
@@ -22,4 +20,14 @@ if __name__ == "__main__":
         print("Not found")
     elif res.status_code == 200:
         res = res.json()
-        print(res['location'])
+        
+        # Print all the data to check the structure
+        print(res)
+
+        # Assuming the structure contains detailed data, print location
+        # Change this based on your actual response structure
+        if 'location' in res:
+            print(res['location'])
+        else:
+            # If 'location' key doesn't exist, look for other keys
+            print("Location not found in the response")
